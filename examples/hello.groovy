32f3ce061824831ec1a53b8209e29c8a80664e35
David Kellum <dek-oss@gravitext.com>
2009-02-16 10:46:41 -0800

    hashdot.header.comment = "//" in groovy.hdp and rhino.hdp, plus update examples

--- examples/hello.groovy
+++ examples/hello.groovy
@@ -1,3 +1,12 @@
 #!/usr/bin/env groovy
+// Must find symlink groovy -> hashdot* in path, since groovy.hdp 
+// sets hashdot.header.comment = "//" for below to work:
+//.test.prop = "hello world!"
+//
 
-println "Hello World!"
+msg = System.getProperty( "test.prop" )
+if( msg == null ) {
+    throw new RuntimeException( "Property 'test.prop' not set" )
+}
+
+println( msg )
--- examples/hello.js
+++ examples/hello.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env rhino
+// Must find symlink rhino -> hashdot* in path, since rhino.hdp 
+// sets hashdot.header.comment = "//" for below to work:
+//.test.prop = "hello world!"
+//
 
-print('hello world')
+msg = java.lang.System.getProperty( "test.prop" )
+if( msg == null ) {
+    throw "Property 'test.prop' not set"
+}
+
+print( msg )
